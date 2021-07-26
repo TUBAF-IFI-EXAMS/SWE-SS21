b0d73096e2a1fbb5f4fae3cb5af67004961ac7c8
gelbeforelle <84494077+gelbeforelle@users.noreply.github.com>
2021-07-26 23:15:10 +0200

    Update GUI
    - main purpose is now input of file paths for IO
    - rename Form1 to MainForm
    - Form2 is not used anymore
    - current example program takes a .docx file and converts all text for QTI format

---
+++ Explorer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+static class Explorer
+    {
+        public static string Open(string Startup_Path, string Filter)
+    {
+        string filePath = string.Empty;
+        //code from Microsoft documentation
+        //explorer-like file selection
+        var openFileDialog = new OpenFileDialog();
+        openFileDialog.InitialDirectory = Startup_Path;
+        openFileDialog.Filter = Filter;
+        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            //Get the path of specified file
+            filePath = openFileDialog.FileName;
+
+        }
+        return filePath;
+    }
+    }
+
